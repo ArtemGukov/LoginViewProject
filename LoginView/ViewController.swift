a1ef6ea32d41a84f94fc8f502f5731671b2cd94c
Artem Gukov <artem.gukov@gmail.com>
2019-03-11 21:17:38 +0300

    Initial Commit

---
+++ LoginView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginView
+//
+//  Created by Артем on 11/03/2019.
+//  Copyright © 2019 Gukov.space. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
